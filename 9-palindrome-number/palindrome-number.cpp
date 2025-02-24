11cc8b59af4f27a3523992ed7c562decc46ece35
Srimeghana33 <meghanamullapudi33@gmail.com>
2025-02-24 15:18:55 +0530

    Time: 2 ms (52.86%) | Memory: 8.7 MB (26.36%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        long rev=0,num=x;
+        while(x>0){
+            int lastdigit=x%10;
+            rev=(rev*10)+lastdigit;
+            x=x/10;
+        }
+        return rev==num;
+    }
+};
